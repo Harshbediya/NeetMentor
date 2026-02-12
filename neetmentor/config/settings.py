39d76566b67a357b30dc0f468a1fdaa7e37bf7e8
Harsh Bediya <harshbedi3112@gmail.com>
2026-02-12 23:33:36 +0530

    Switch to Gmail SSL (Port 465) for better compatibility with Render

--- neetmentor/config/settings.py
+++ neetmentor/config/settings.py
@@ -148,8 +148,9 @@ AUTH_USER_MODEL = 'core.User'
 # Email Settings for Production (Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465  # Use SSL Port
+EMAIL_USE_SSL = True  # Use SSL
+EMAIL_USE_TLS = False # Disable TLS
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = f'NEETMentor <{EMAIL_HOST_USER}>'
